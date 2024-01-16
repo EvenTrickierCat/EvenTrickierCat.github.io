a7a2db9f5acd58b565d9743b96628e720cbfad5f
Even Trickier Cat <151370006+EvenTrickierCat@users.noreply.github.com>
2024-01-16 22:09:50 +0200

    update

--- .github/workflows/scripts/posts-google-search.sh
+++ .github/workflows/scripts/posts-google-search.sh
@@ -1,4 +1,17 @@
-echo Hello from custom file! 👋
+# filename => path
+# _posts/2022-03-20-hello-world.md => 2022/03/20/hello-world.html
+function filenameToUrlSlug(){
+$fileName=$1
 
+# echo $s | awk -F/ '{ print $2 }'
+# echo $s | awk -F. '{ print $1 }' | awk -F/ '{ print $2 }' | awk -F- '{ print $1"/"$2"/"$3"/" }'
+# echo $s | awk -F. '{ print $1 }' | awk -F/ '{ print $2 }' | awk -F- '{ printf "%s/%s/%s/", $1, $2, $3 ; for(i=4; i<=NF; i++) { printf "%s", $i; if(i<NF){ printf "-" } else { printf ".html" } } }'
+
+echo $fileName | awk -F. '{ print $1 }' | awk -F/ '{ print $2 }' | awk -F- '{ printf "%s/%s/%s/", $1, $2, $3 ; for(i=4; i<=NF; i++) { printf "%s", $i; if(i<NF){ printf "-" } else { printf ".html" } } }'
+
+}
+
+echo Hello from custom file! 👋
+echo $ALL_CHANGED_POST_FILES
 
 
